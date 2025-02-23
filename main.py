9cf06c907721905e98190e9b7d6c5cfa2fd577a2
swastik pingale <sojupingale@gmail.com>
2025-02-23 20:14:35 +0530

    Version 1

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Ask user for password length
+length = int(input("Enter password length: "))
+password = generate_password(length)
+print("Generated Password:", password)
